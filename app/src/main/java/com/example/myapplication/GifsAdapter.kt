8aa34db86fb971de38a88ac080f573bd4084f228
max19 <max1998sasd@gmail.com>
2023-09-01 15:29:31 +0300

    update GifsAdapter.kt

--- app/src/main/java/com/example/myapplication/GifsAdapter.kt
+++ app/src/main/java/com/example/myapplication/GifsAdapter.kt
@@ -9,12 +9,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 class GifsAdapter(val context: Context, val gifs: List<DataObject>) : RecyclerView.Adapter<GifsAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    lateinit var mListener: OnItemClickListener
+    interface OnItemClickListener{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        mListener = listener
+    }
+    class ViewHolder(itemView: View,listener: OnItemClickListener): RecyclerView.ViewHolder(itemView) {
         val imageView = itemView.findViewById<ImageView>(R.id.ivGif)
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_layout,parent, false))
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_layout,parent, false), mListener)
     }
 
     override fun getItemCount(): Int {
