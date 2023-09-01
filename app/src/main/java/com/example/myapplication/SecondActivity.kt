85d2268308f8727304280ce165fc8cc89f746310
max19 <max1998sasd@gmail.com>
2023-09-01 15:30:16 +0300

    update SecondActivity.kt

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val url = intent.getStringExtra("url")
+
+        Glide.with(this).load(url).into(imageView)
     }
 }
